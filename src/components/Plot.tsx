3df2cc2272fdb85666fe20d59d0208dd1e49ed98
Aditya <adijr9487@gmail.com>
2026-01-03 07:35:17 +0530

    replaced lib antd graph with recharts, updated the mapping for chart plotting

--- src/components/Plot.tsx
+++ src/components/Plot.tsx
@@ -1,12 +1,31 @@
-import { Column } from "@ant-design/charts";
 import { Card } from "antd";
 
 import { AggregateInsight, Insight } from "../api/types";
 
 import {
-  mapAggregateInsightToPlotData,
-  mapInsightToPlotData,
-} from "../utils/helper";
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const COMMON_METRICS = ["ctr", "cpc", "conversion_rate"] as const;
+
+const mapInsightsToRechartData = (
+  aggregate: AggregateInsight | undefined,
+  insight: Insight | undefined
+) => {
+  return COMMON_METRICS.map((key) => ({
+    name: key.replace(/_/g, " ").toUpperCase(),
+    aggregate: aggregate?.insights[
+      `avg_${key}` as keyof AggregateInsight["insights"]
+    ] as number,
+    insight: insight?.insights[key],
+  }));
+};
 
 export const Plot = ({
   aggregateInsightData,
@@ -17,27 +36,48 @@ export const Plot = ({
   specificInsightData: Insight | undefined;
   isLoading: boolean | undefined;
 }) => {
-  const data = [];
-  if (specificInsightData) {
-    data.push(...mapInsightToPlotData(specificInsightData));
-  }
-  if (aggregateInsightData) {
-    data.push(...mapAggregateInsightToPlotData(aggregateInsightData));
-  }
+  const data = mapInsightsToRechartData(
+    aggregateInsightData,
+    specificInsightData
+  );
 
-  const config = {
-    data,
-    height: 250,
-    xField: "label",
-    yField: "value",
-    colorField: "name",
-    group: {
-      padding: 0,
-    },
-  };
   return (
     <Card size="small" title="Plot" loading={isLoading}>
-      <Column {...config} />
+      <BarChart
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+          maxHeight: 254,
+          aspectRatio: 1.618,
+        }}
+        responsive
+        data={data}
+        margin={{
+          top: 20,
+          right: 0,
+          left: 0,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis
+          yAxisId="aggregate"
+          orientation="left"
+          stroke="#8884d8"
+          width="auto"
+        />
+        <YAxis
+          yAxisId="insight"
+          orientation="right"
+          stroke="#82ca9d"
+          width="auto"
+        />
+        <Tooltip />
+        <Legend />
+        <Bar yAxisId="aggregate" dataKey="aggregate" fill="#8884d8" />
+        <Bar yAxisId="insight" dataKey="insight" fill="#82ca9d" />
+      </BarChart>
     </Card>
   );
 };
